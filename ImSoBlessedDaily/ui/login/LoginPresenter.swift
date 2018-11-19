a9333e91ef1a69a1600d9730ccf915908e37715a
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2018-11-20 04:52:55 +0800

    Awesome design pattern and project organization

---
+++ ImSoBlessedDaily/ui/login/LoginPresenter.swift
@@ -0,0 +1,16 @@
+//
+//  VCLoginPresenter.swift
+//  ImSoBlessedDaily
+//
+//  Created by Lainel John Dela Cruz on 20/11/2018.
+//  Copyright © 2018 Lainel John Dela Cruz. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+class LoginPresenter{
+    
+}
---
+++ ImSoBlessedDaily/ui/login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  ImSoBlessedDaily
+//
+//  Created by Lainel John Dela Cruz on 17/11/2018.
+//  Copyright © 2018 Lainel John Dela Cruz. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var UIEmailTF: UITextField!
+    @IBOutlet weak var UIPasswordTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
